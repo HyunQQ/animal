5c6d907019bb491902d3e7ebd6538fffbd093cb2
5hyunq <qqkqqk23@gmail.com>
2020-10-28 09:08:56 +0900

    api 초안

--- api/animal_animal.py
+++ api/animal_animal.py
@@ -3,15 +3,100 @@ sample
 http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/sido?serviceKey=[secret key]
 """
 
-from urllib2 import Request, urlopen
-from urllib import urlencode, quote_plus
+import configparser
+from urllib.request import urlopen
+from urllib.parse import urlencode
 
-url = 'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/sido'
-queryParams = '?' + urlencode({ quote_plus('ServiceKey') : '서비스키' })
+config = configparser.ConfigParser()
+config.read('config/config.ini')
 
-request = Request(url + queryParams)
-request.get_method = lambda: 'GET'
-response_body = urlopen(request).read()
-print response_body
 
+def get_sido():
+    url = config['API']['URL_SIDO']
+
+    input_data = dict()
+    input_data['serviceKey'] = config['API']['APP_KEY']
+    url_values = urlencode(input_data)
+
+    full_url = url+'?'+url_values
+    print(full_url)
+
+    response = urlopen(full_url)
+    print(response)
+
+    rslt=0
+    return rslt
+
+
+def get_sigungu(sido):
+    url = config['API']['URL_SIGUNGU']
+
+    input_data = dict()
+    input_data['upr_cd'] = sido
+    input_data['serviceKey'] = config['API']['APP_KEY']
+    url_values = urlencode(input_data)
+
+    full_url = url+'?'+url_values
+    print(full_url)
+
+    response = urlopen(full_url)
+    print(response)
+
+    rslt = 0
+    return rslt
+
+def get_shelter(sido, sigungu):
+    url = config['API']['URL_SHELTER']
+
+    input_data = dict()
+    input_data['upr_cd'] = sido
+    input_data['org_cd'] = sigungu
+    input_data['serviceKey'] = config['API']['APP_KEY']
+    url_values = urlencode(input_data)
+
+    full_url = url+'?'+url_values
+    print(full_url)
+
+    response = urlopen(full_url)
+    print(response)
+
+    rslt = 0
+    return rslt
+
+def get_kind(up_kind_cd):
+    url = config['API']['URL_KIND']
+
+    input_data = dict()
+    input_data['up_kind_cd'] = up_kind_cd
+    input_data['serviceKey'] = config['API']['APP_KEY']
+    url_values = urlencode(input_data)
+
+    full_url = url+'?'+url_values
+    print(full_url)
+
+    response = urlopen(full_url)
+    print(response)
+    rslt = 0
+    return rslt
+
+def get_abandonment(bgnde, endde, pageNo, numOfRows):
+    url = config['API']['URL_ABANDONMENT']
+
+    input_data = dict()
+    input_data['bgnde'] = bgnde
+    input_data['endde'] = endde
+    input_data['pageNo'] = pageNo
+    input_data['numOfRows'] = numOfRows
+
+    input_data['serviceKey'] = config['API']['APP_KEY']
+    url_values = urlencode(input_data)
+
+    full_url = url+'?'+url_values
+    print(full_url)
+
+    response = urlopen(full_url)
+    print(response)
+
+    rslt = 0
+    return rslt
 
