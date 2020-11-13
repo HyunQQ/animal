e1650fb11fc22f5f15edce23790c9250d1cd72d6
5hyunq <qqkqqk23@gmail.com>
2020-11-13 09:28:32 +0900

    fix setting

--- animal/settings.py
+++ animal/settings.py
@@ -26,17 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config['DJANGO']['SECRET_KEY']
-# SECRET_KEY='%t1b04e$&_)7$^3@235v_fi)^yl-am%zfh9*gpubd+0s!s-1h('
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
