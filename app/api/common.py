d2c145038aea1506b146f12d5dd47d35ae90c1e6
5hyunq <qqkqqk23@gmail.com>
2020-11-16 11:05:23 +0900

    add korean url handling

--- app/api/common.py
+++ app/api/common.py
@@ -1,3 +1,7 @@
+import re
+from urllib.parse import quote_plus
+
+
 def get_querys(request):
     rslt = dict()
     for key, value in request.query_params.items():
@@ -14,11 +18,17 @@ def make_response(info_data: dict, response_data: dict=dict(), req_param: dict= 
 
     return response
 
+
 def make_url(basic_url: str, query_data: dict):
     full_url = basic_url + "?"
     len_query = len(query_data)
 
+
     for index, (key, value) in enumerate(query_data.items()):
+
+        if re.compile('[ㄱ-ㅎㅏ-ㅣ가-힣]+').search(value) != None:
+            value = quote_plus(value)
+
         if index == len_query-1:
             full_url += key + "=" + value
         else:
--- config/app_config.py
+++ config/app_config.py
@@ -5,6 +5,8 @@ from config import DevConfig, ProdConfig
 class AppConfig():
     def __init__(self):
         if DEBUG == True:
-            DevConfig.__init__(self)
+            # DevConfig.__init__(self)
+            pass
         else:
-            ProdConfig.__init__(self)
+            # ProdConfig.__init__(self)
+            pass
