648de4b063943c7fb851c084dcecc8eb0074d6b2
5hyunq <qqkqqk23@gmail.com>
2020-11-20 09:10:06 +0900

    add view test

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,25 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
 
-class AnimalTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
+class AnimalViewTest(TestCase):
+    def test_response_sido(self):
+        response = self.client.get('/sido/')
+        self.assertEqual(response.status_code, 200)
 
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
+    def test_response_sigungu(self):
+        response = self.client.get('/sigungu/?upr_cd=6110000')
+        self.assertEqual(response.status_code, 200)
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+    def test_response_shelter(self):
+        response = self.client.get('/shelter/?upr_cd=6110000&org_cd=3220000')
+        self.assertEqual(response.status_code, 200)
 
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
+    def test_response_shelter_detail(self):
+        response = self.client.get('/shelter_detail/?care_reg_no=311322200900001')
+        self.assertEqual(response.status_code, 200)
 
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
+    def test_response_kind(self):
+        response = self.client.get('/kind/?up_kind_cd=417000')
+        self.assertEqual(response.status_code, 200)
+
+    def test_response_abandonment(self):
+        response = self.client.get('/abandonment/?numOfRows=10&pageNo=1&bgnde=20140601&endde=20140630&upkind=417000&kind=000054&upr_cd=6110000&org_cd=3220000&care_reg_no=311322200900001&state=notice&neuter_yn=Y')
+        self.assertEqual(response.status_code, 200)
+
+
+
