735b185c118dc0e84e477571f858f7360a92eb3a
5hyunq <qqkqqk23@gmail.com>
2020-12-22 15:52:53 +0900

    add shelter_nearby from address

--- app/api/addon_api.py
+++ app/api/addon_api.py
@@ -1,12 +1,33 @@
-def get_nearby_shelter(location):
+from app.models.locations import Sido, Sigungu
+from app.api.animal_api import get_shelter
+from app.common.result import make_response_content
+
+
+def get_shelter_nearby(querys: dict):
     # ex location : 경기도 양주시 남면 감악산로 63-37 (남면) 한국동물구조관리협회
+    # 경상남도 창원시 마산합포구 진북면 지산2길 139-112 226-19
+    location = querys['location']
     sub_location = location.split()
     sido = sub_location[0]
     sigungu = sub_location[1]
 
+    if "창원" in sigungu:
+        sigungu = sub_location[1][:2] + " " + sub_location[2][:2]
 
 
-    return 0
+    try:
+        data = dict()
+        data['upr_cd'] = Sido.objects.values_list('sidoCd', flat=True).filter(sidoNm__contains=sido)[0]
+        data['org_cd'] = Sigungu.objects.values_list('sigunguCd', flat=True).filter(sigunguNm__contains=sigungu)[0]
+        response = get_shelter(data)
+
+    except:
+        reponse_data = {
+            'Open API Server Error': 'Cannot get data in DB.'
+        }
+        response = make_response_content(response_data=reponse_data, req_param=querys)
+
+    return response
 
 
 def get_action_guide():
--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 from app.api.views import UserViewSet, AnimalUserViewSet
-from app.api.views import sido, sigungu, shelter, shelter_detail, kind, kind_cd,  abandonment, set_default_db
+from app.api.views import sido, sigungu
+from app.api.views import shelter, shelter_detail, shelter_nearby
+from app.api.views import kind, kind_cd, abandonment, set_default_db
 
 app_name='app'
-
 
 router = routers.DefaultRouter()
 router.register(r'basicusers', UserViewSet)
@@ -18,6 +19,7 @@ urlpatterns=[
     path('sigungu/', sigungu),
     path('shelter/', shelter),
     path('shelter_detail/', shelter_detail),
+    path('shelter_nearby/', shelter_nearby),
     path('kind_cd/', kind_cd),
     path('kind/', kind),
     path('abandonment/', abandonment)
